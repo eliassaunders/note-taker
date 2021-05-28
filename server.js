751e6cddb0894a546b152e6ae8c818cf8a8a0f2f
Eliadad <eliasashtonsaunders@gmail.com>
2021-05-27 21:11:06 -0700

    heroku push

--- server.js
+++ server.js
@@ -1,22 +1,29 @@
-const { rejects } = require('assert');
+const addRequestId = require('express-request-id')();
 const express = require('express');
 const fs = require('fs');
 const path = require('path');
+const { v4: uuidv4 } = require('uuid');
+
 const { send } = require('process');
+const { rejects } = require('assert');
 
 const PORT = process.env.PORT || 3001;
 const app = express();
 
+
+
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
+app.use(express.static('public'))
+app.use(addRequestId);
 
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, './public/index.html'));
+    res.sendFile(path.join(__dirname, './index.html'));
 });
 
 
 app.get('/notes', (req, res) => {
-   res.sendFile(path.join(__dirname, './public/notes.html'));
+   res.sendFile(path.join(__dirname, './notes.html'));
 });
 
 app.get('/api/notes', (req, res) => {
@@ -30,9 +37,27 @@ app.get('/api/notes', (req, res) => {
 })
 
 app.post('/api/notes', (req, res) => {
-    const note = JSON.stringify(req.body)
+    
+    const newNote = {
+        ...req.body,
+        id: uuidv4()
+    }
 
-    fs.writeFileSync('./db/db.json', note, err => {
+    const info = [];
+
+    fs.readFile('./db/db.json', 'utf-8', (err, data) => {
+        if (err) {
+            req.status(400)
+            return;
+        }
+        info.push(data);
+    })
+
+    info.push(newNote);
+
+    const stringed = JSON.stringify(info);
+
+    fs.writeFile('./db/db.json', stringed, err => {
         if (err) {
             res.status(400)
             return;
